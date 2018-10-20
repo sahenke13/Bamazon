ffa9945917aae81b46b154d72c3d8000e00f882b
sahenke13 <shenke13@gmail.com>
2018-10-20 15:52:48 -0500

    bamazonV2.1

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,12 +17,82 @@ connection.connect(function(err) {
   });
 
 function ManagerChoice(){
-    inquirer.Prompt([
+    inquirer.prompt([
         {
         type: "list",
         message: "Hello, Manager please make your choice",
-        choice: 
+        choices: ["Products for sale", "View low inventory", "Add to inventory", "Add new products"],
+        name: "choice"
+        }
+        ]).then(function(response){
+
+            let option = response.choice;
+
+            switch(option){
+                case "Products for sale": 
+                    console.log("Products for sale");
+                    Products();
+                    break;
+                case "View low inventory":
+                    console.log("Low Inventory")
+                    LowInventory();
+                    break;
+                case "Add to inventory":
+                    console.log("Add to Inventory");
+                    break;
+                case "Add new products":
+                    console.log("Add new products");
+                default:
+                    console.log("something has gone terrible wrong.")
+            }
+          
+        }) 
+}
+ManagerChoice();
+
+
+//lets make some fun functions
+// List Products function
+function Products(){
+    connection.query("SELECT * from products", function(err, res){
+        if(err) throw err;
+        console.log("\nThese are the items Bamazon has for sale");
+        for (let i in res){
+            let stuff = res[i];
+
+            console.log(
+                "\nid: ", stuff.id,
+                "Name: ",stuff.product_name,
+                "Prices: ", stuff.price, 
+                "Quantity: ", stuff.stock_quantity
+            )
+        }
+    })
+  }
+
+  // Low Inventoty function
+function LowInventory(){
+        connection.query("SELECT * from products WHERE stock_quantity<5", function(err, res){
+            if(err) throw err;
+            for(let i in res){
+            console.log("This product is low: " + res[i].product_name);
+            }
+        })
+  }
+
+  //Add to Inventory function
+function AddInventory(){
+        inquirer.prompt([
+        {
+        type: "input",
+        message: "What items stock would you like to increase"
+
+
+
 
         }
     ])
+
+
+
 }
