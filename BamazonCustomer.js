c475e97699d2fd28a4c8dce1231675ae11d725d2
sahenke13 <shenke13@gmail.com>
2018-10-20 14:13:48 -0500

    BamazonV1.3

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -26,39 +26,61 @@ connection.connect(function(err) {
                 "\nid: ", stuff.id,
                 "Name: ",stuff.product_name,
                 "Prices: ", stuff.price, 
+                "Quantity: ", stuff.stock_quantity
             )
         }
+
     })
   }
 
   function userChoice(){    
       inquirer.prompt([
           {
-        type: "input",
         message: "Pick the Items ID you would like to purchase",
         name: "itemID"
           },
           {
-        type: "input",
         message: "How many would you like to buy?",
         name: "quantityItem"
           }
       ]).then(function(response){
-        console.log(response.itemID, response.quantityItem)
-        if(response.quantityItem<stuff.stock_quantity){
-            console.log("We have enough for you to purchase")
+            let itemID = response.itemID;
+            let quantityItem = response.quantityItem;
+            console.log(itemID);
+            console.log(quantityItem)
 
-        }else
-        {
-            console.log("We do not have enough for your purchase.")
-        }
+        connection.query("SELECT stock_quantity, price FROM products WHERE id="+ response.itemID, function(err, res){
+            if (err) throw err;
 
+            if(quantityItem <= res[0].stock_quantity){
 
+                console.log("We have enough for you to purchase");
+                let cost =  (res[0].price) * (quantityItem);
+                console.log("The order total will be: $"+ cost);
+                let newquantityItem = res[0].stock_quantity - quantityItem;
+
+        connection.query("UPDATE products SET ? WHERE ?", 
+        [{stock_quantity: newquantityItem},
+            {id: itemID}],
+        function(err){
+            if(err) throw err;
+
+            Products();
+        })
+
+            }
+            else
+            {
+                console.log(res[0].stock_quantity);
+                console.log(res[0].price);
+                console.log("We apologize, we currently do not have enough stock for your order.");
+            }
+            
+        })
+    
       })
-
 
 
   }
   Products();
   userChoice();
-  connection.end();
