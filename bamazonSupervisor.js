870e0c3f4df1302d7c341def1ca254996fb01427
sahenke13 <shenke13@gmail.com>
2018-10-22 15:49:29 -0500

    Bamazon V3.4 Got the table working

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
+// var table = new Table({
+//     head: ['DepartMent ID', 'DepartMent Name', 'Over Head Cost', 'Total Sales', 'Profit']
+//   , colWidths: [20, 20, 20, 20, 20]
+// });
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,6 +17,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    SuperVisorChoice();
   });
 
 function SuperVisorChoice(){
@@ -29,7 +35,7 @@ function SuperVisorChoice(){
         switch (option){
             case "View sales by department":
                 console.log("View sales by department");
-
+                ViewSales();
                 break;
             case "Create a new department":
                 console.log("Create a new department");
@@ -43,11 +49,35 @@ function SuperVisorChoice(){
 //More functions
 
 function ViewSales(){
-    connection.query("SELECT department_id as Dept. ID, department_name as Dept. Name, over_head_cost as Over Head Cost, total_sales AS Total Sales, (total_sales - over_head_cost) AS total_profit FROM departments")
-
-
+    connection.query("SELECT departments.department_id AS ID, products.department_name AS Department, departments.over_head_cost AS OverHead, SUM(product_sales) AS Total_Sales, (SUM(product_sales) - departments.over_head_cost ) AS Total_Profit FROM departments JOIN products ON departments.department_name = products.department_name GROUP BY Department", function(err,res){
+        if(err) throw err;
+        
+        console.table(res);
+    });
 }
 
+function newDept(){
+    inquirerPrompt([
+        {
+        type: "input",
+        message: "What is the name of the department you would like to add?",
+        name: "newDept"
+        },
+        {
+        type: "input",
+        message: "What is the over head cost of this department?",
+        name: "newOverHeadCost"
+        }
+    ]).then(function(supAnswers){
+        let newDeptName = supAnswers.newDept;
+        let newDeptOverHead = supAnswers.newDeptOverHead;
 
 
-SuperVisorChoice();
+
+
+
+
+    })
+
+};
+
